95b371ce3df4f1e2aa59ee43958c2fc5386dae37
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-05-30 14:52:56 -1000

    secret number practice

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,15 @@
 'use strict';
 module.exports = function() {
-  
+
+	var secretNumber = Math.floor(Math.random() * 1000000 - 1) + 1;
+
+	function test(){
+
+		return secretNumber;
+
+
+	}
+	
+	return test;
+
 };
